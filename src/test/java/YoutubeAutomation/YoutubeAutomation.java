b3e598b69376f4e4ce5837a418f2d9f82b56c5f1
Prashant Udale <prashantudale@gmail.com>
2024-07-24 17:46:29 +0530

    Project Created

---
+++ src/test/java/YoutubeAutomation/YoutubeAutomation.java
@@ -0,0 +1,14 @@
+package YoutubeAutomation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class YoutubeAutomation {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.youtube.com/");
+
+	}
+
+}
