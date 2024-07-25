c236f0a9d5ca25a9fffbfab9a0b794d6a4666b7b
Prashant Udale <prashantudale@gmail.com>
2024-07-25 20:20:43 +0530

    Updated

--- src/test/java/YoutubeAutomation/YoutubeAutomation.java
+++ src/test/java/YoutubeAutomation/YoutubeAutomation.java
@@ -1,5 +1,8 @@
 package YoutubeAutomation;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YoutubeAutomation {
@@ -12,12 +15,22 @@ public class YoutubeAutomation {
 		//Launch the URL and maximize
 		driver.get("https://www.youtube.com/");
 		
+		//Maximize and Implicit Wait for things to initialize
+	    driver.manage().window().maximize();
+	    //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
 		//Set item name here
-		String searchWord = "Movies";
+		String searchWord = "Kannada Movies";
 		
 		// Wait 5 seconds for site to get loaded
-	    Thread.sleep(5000);
+	    //Thread.sleep(10000);
 		
+	    //
+	    driver.findElement(By.xpath("//input[@id='search']")).clear();
+	    driver.findElement(By.xpath("//input[@id='search']")).sendKeys(searchWord);
+	    //driver.findElement(By.name("search_query")).clear();
+	    //driver.findElement(By.name("search_query")).sendKeys(searchWord);
+	    
 
 	}
 
