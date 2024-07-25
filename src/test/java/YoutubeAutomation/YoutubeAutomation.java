04a084efc0da0a2d070960b9158a1f56690aae74
Prashant Udale <prashantudale@gmail.com>
2024-07-25 17:28:57 +0530

    launch browser and get url

--- src/test/java/YoutubeAutomation/YoutubeAutomation.java
+++ src/test/java/YoutubeAutomation/YoutubeAutomation.java
@@ -4,10 +4,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YoutubeAutomation {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Launch Chrome Browser
 		ChromeDriver driver = new ChromeDriver();
 		
+		//Launch the URL and maximize
 		driver.get("https://www.youtube.com/");
+		
+		//Set item name here
+		String searchWord = "Movies";
+		
+		// Wait 5 seconds for site to get loaded
+	    Thread.sleep(5000);
+		
 
 	}
 
