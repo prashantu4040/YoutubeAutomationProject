0776109878a464d4e07bceecdd8917a9217f3668
Prashant Udale <prashantudale@gmail.com>
2024-07-28 00:26:27 +0530

    Update

--- src/test/java/YoutubeAutomation/YoutubeAutomation.java
+++ src/test/java/YoutubeAutomation/YoutubeAutomation.java
@@ -40,27 +40,32 @@ public class YoutubeAutomation {
 		driver.findElement(By.xpath("//button[@aria-label='Search']")).click();
 
 		// Wait 5 seconds for site to get loaded
-		Thread.sleep(5000);
+		//Thread.sleep(5000);
 
 		// Get movie name and print it in console
 		// driver.findElement(By.xpath("(//a[@id='video-title'])[1]")).getText();
-		String movieOne = driver.findElement(By.xpath("(//a[@id='video-title'])[1]")).getText();
+		/*String movieOne = driver.findElement(By.xpath("(//a[@id='video-title'])[1]")).getText();
 		String movieTwo = driver.findElement(By.xpath("(//a[@id='video-title'])[2]")).getText();
 		String movieThree = driver.findElement(By.xpath("(//a[@id='video-title'])[2]")).getText();
 		System.out.println("Movie 1: " + movieOne);
 		System.out.println("Movie 2: " + movieTwo);
-		System.out.println("Movie 3: " + movieThree);
+		System.out.println("Movie 3: " + movieThree);*/
 		
 		// Wait 5 seconds
-	    Thread.sleep(5000);
+	    //Thread.sleep(5000);
 	    
-	 // Wait for the results to load and get movie names
+	    // Wait for the results to load and get movie names
 	    List<WebElement> videoResults = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//a[@id='video-title']")));
 
 
-	 // Print the total number of video results
+	    // Print the total number of video results
         int numberOfResults = videoResults.size();
         System.out.println("Total number of movie results: " + numberOfResults);
+        
+        //Updated the code to print first 3 movies
+        for (int i = 0; i < Math.min(3, numberOfResults); i++) {
+            System.out.println("Movie " + (i + 1) + ": " + videoResults.get(i).getText());
+        }
         
 	    //Close and quit the driver
 	    //driver.close();
